b7f336e1fc42cfce4de03925ed322a3b09df0a05
Albert-cmd <albert1999dual@gmail.com>
2019-10-16 18:23:34 -0700

    Primer commit: Creacion de clases y projectes

---
+++ Kontroller/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontroller
+{
+    public class Class1
+    {
+    }
+}
